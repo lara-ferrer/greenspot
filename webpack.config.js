3109d16ebb64c01af82cd8600ae275273c568996
lara-ferrer <larafpics@gmail.com>
2024-01-07 20:55:59 +0100

    [feat]: modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
-const path = require('path');
+import path from path;
+import node from "file.node";
 
 module.exports = {
     entry: './src/App.tsx',
+    target: "node",
+    externals: [nodeExternals()],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
         rules: [
-            {test: /\.ts$/, use: 'ts-loader'}
+            {test: /\.ts$/, use: 'ts-loader'},
+            {
+                test: /\.node$/,
+                loader: "node-loader",
+              },
         ]
     },
     resolve: {
